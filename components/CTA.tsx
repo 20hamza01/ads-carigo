f1caaf5403fca673751b7401dc920fb1b4439267
20hamza01 <90298569+20hamza01@users.noreply.github.com>
2025-07-18 20:35:40 +0100

    Set Event API

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -1,4 +1,4 @@
-// "use client";
+"use client";
 
 import React from "react";
 import { Button } from "./ui/button";
@@ -6,24 +6,24 @@ import Link from "next/link";
 import Image from "next/image";
 
 const CTA = () => {
-  // const handleWhatsAppClick = async () => {
-  //   await fetch("/api/tiktok", {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify({
-  //       url: window.location.href,
-  //       referrer: document.referrer,
-  //     }),
-  //   });
+  const handleWhatsAppClick = async () => {
+    await fetch("/api/tiktok", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        url: window.location.href,
+        referrer: document.referrer,
+      }),
+    });
 
-  //   // Then redirect to WhatsApp
-  //   window.open(
-  //     "https://wa.me/+212715192639?text=سلام%20👋%D8%8C%20بغيت%20نستافد%20من%20العرض",
-  //     "_blank"
-  //   );
-  // };
+    // Then redirect to WhatsApp
+    window.open(
+      "https://wa.me/+212715192639?text=سلام%20👋%D8%8C%20بغيت%20نستافد%20من%20العرض",
+      "_blank"
+    );
+  };
 
   return (
     <div className="flex items-center justify-center flex-col gap-4">
@@ -35,7 +35,7 @@ const CTA = () => {
         <Button
           asChild
           className="bg-green-500 font-bold hover:bg-green-600 cursor-pointer"
-          // onClick={handleWhatsAppClick}
+          onClick={handleWhatsAppClick}
         >
           <div>
             بغيتي تحجز؟ كليكي هنا و راسلنا دابا
